8cef76113caad73643acc67d60bb43a970fcc838
Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-05-14 15:24:37 +0630

    new built

--- order-service/src/main/java/net/farout/orderservice/repository/OrderRepo.java
+++
@@ -1,7 +0,0 @@
-package net.farout.orderservice.repository;
-
-import net.farout.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepo extends JpaRepository<Order, Long> {
-}
--- product-service/src/main/java/net/farout/micro_api/repository/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package net.farout.micro_api.repository;
-
-import net.farout.micro_api.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepo extends MongoRepository<Product, String> {
-}
