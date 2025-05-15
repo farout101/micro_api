9fc05ba5e5ce422039599e264cbb4cc6c886dfdb
Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-05-15 09:18:57 +0630

    new version

--- micro-api/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ micro-api/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -6,7 +6,9 @@ import com.programmingtechie.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,11 +17,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        var order = mapToOrder(orderRequest);
-        orderRepository.save(order);
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems>
     }
+
+    private OrderLineItems mapToDto (OrderLineItemsDto orderLineItemsDto) {
+        OrderLine Items orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice (orderLine Items Dto.getPrice());
+        orderLineItems.setQuantity (orderLine Items Dto.getQuantity());
+        orderLineItems.setSkuCode (orderLine Items Dto.getSkuCode());
+        return orderLineItems;
 
     private static Order mapToOrder(OrderRequest orderRequest) {
         Order order = new Order();
